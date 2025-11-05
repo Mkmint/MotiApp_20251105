1645a50cde4baacd9f89d209159e4a5fa89f390f
Mkmint <dpazpdlah@gmail.com>
2025-11-05 20:46:00 +0900

    motivation 객체 부여

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,10 +1,14 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class App {
 
     Scanner sc;
+
+    List<Motivation> motivationList = new ArrayList<>();
 
     public App(Scanner sc) {
         this.sc = sc;
@@ -14,7 +18,7 @@ public class App {
         System.out.println("==motivation App 실행==");
 
         while(true){
-            System.out.print("명령어) ");
+            System.out.println("명령어) ");
             String cmd = sc.nextLine().trim();
 
             if(cmd.equals("exit")){
@@ -30,16 +34,19 @@ public class App {
             if(cmd.equals("add")) {
                 System.out.print("내용 : ");
                 String motiv = sc.nextLine().trim();
-                System.out.println("저자 : ");
+                System.out.print("저자 : ");
                 String writer = sc.nextLine().trim();
                 System.out.println("명언 생성 완료");
+
+                Motivation motivation = new Motivation(1, motiv, writer);
+
+                motivationList.add(motivation);
+
             }
             if(cmd.equals("list")){
                 System.out.println("번호 / 명언 / 저자 ");
-                System.out.printf(" 1    / %s", "%^&*/ !^#\n");
+                System.out.printf(motivationList.toString());
             }
-
-
         }
 
 
