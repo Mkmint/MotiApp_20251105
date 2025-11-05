ab59d01ec4f1e05cd78aad7916692845e2c9ec0e
Mkmint <dpazpdlah@gmail.com>
2025-11-05 21:17:32 +0900

    번호 자동증가 & 명언 없는 경우

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -15,8 +15,8 @@ public class App {
     }
 
     public void run(){
+        int lastId = 0;
         System.out.println("==motivation App 실행==");
-
         while(true){
             System.out.println("명령어) ");
             String cmd = sc.nextLine().trim();
@@ -32,20 +32,27 @@ public class App {
             }
 
             if(cmd.equals("add")) {
+                lastId++;
                 System.out.print("내용 : ");
                 String motiv = sc.nextLine().trim();
                 System.out.print("저자 : ");
                 String writer = sc.nextLine().trim();
-                System.out.println("명언 생성 완료");
+                System.out.println((lastId) + "번 명언 생성 완료");
 
-                Motivation motivation = new Motivation(1, motiv, writer);
+                Motivation motivation = new Motivation(lastId, motiv, writer);
 
                 motivationList.add(motivation);
 
             }
             if(cmd.equals("list")){
                 System.out.println("번호 / 명언 / 저자 ");
-                System.out.printf(motivationList.toString());
+
+                if(motivationList.size() == 0) {
+                    System.out.println("해당 사항이 없습니다");
+                } else {
+                    System.out.println("해당사항 발견");
+                }
+                //System.out.printf(motivationList.toString());
             }
         }
 
